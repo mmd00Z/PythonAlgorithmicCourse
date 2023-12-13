324e4d13f4b82fb675e818c6208762e3bf1625b9
MMD <mmd.gh313@gmail.com>
2023-12-13 23:49:21 +0330

    update

--- PSET/PSET_1_1.py
+++ PSET/PSET_1_1.py
@@ -2,7 +2,7 @@ n = int(input())
 
 print('W' + n*'o'+ 'w!')
 
-# print('W' + n*'o'+ 'w!', end='')
+# print('W', end='')
 # for i in range(n):
 #     print('o', end='')
 # print('w!')
--- Week4/Codes/with_func.py
+++ Week4/Codes/with_func.py
@@ -1,22 +1,16 @@
 # With Functions
 
-def calculate_rectangle_area(length, width):
+def calc_rectangle_area(length, width):
     return length * width
 
-def calculate_circle_area(radius):
+def calc_circle_area(radius):
     return 3.14 * radius**2
 
-def calculate_triangle_area(base, height):
+def calc_triangle_area(base, height):
     return 0.5 * base * height
 
-# Calculate and print the area of different shapes
-length = 5
-width = 10
-print("Area of rectangle:", calculate_rectangle_area(length, width))
+print(calc_rectangle_area(10, 5))
 
-radius = 3
-print("Area of circle:", calculate_circle_area(radius))
+print(calc_circle_area(3))
 
-base = 4
-height = 8
-print("Area of triangle:", calculate_triangle_area(base, height))
+print(calc_triangle_area(10, 4))
