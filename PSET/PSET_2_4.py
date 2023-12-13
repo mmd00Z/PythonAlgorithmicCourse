ccf3d9bf03994029fc353d718e291a99698750f5
MMD <mmd.gh313@gmail.com>
2023-12-14 02:35:58 +0330

    add PSET_2_4

---
+++ PSET/PSET_2_4.py
@@ -0,0 +1,9 @@
+for i in range(int(input()), int(input())+1):
+    if i > 1:
+        is_prime = True
+        for j in range(2, i):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(i)
